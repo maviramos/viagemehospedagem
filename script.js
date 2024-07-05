aedf90e4f3a0fe716a60d805f5bb8ccb312f261c
maviramos <vitoriamariaifc@gmail.com>
2024-07-04 21:49:36 -0300

    site inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('contatoForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulário enviado com sucesso!');
+});
+
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Busca realizada com sucesso!');
+});
